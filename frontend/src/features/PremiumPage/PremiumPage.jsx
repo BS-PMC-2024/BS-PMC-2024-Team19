6eb05800946badaae3da3060e1ffdd880ad4972f
jonatanbuga <yhonabo@ac.sce.ac.il>
2024-08-19 19:25:36 +0300

    change layouy of premiumPage jsx and update navegate invest info

--- frontend/src/features/PremiumPage/PremiumPage.jsx
+++ frontend/src/features/PremiumPage/PremiumPage.jsx
@@ -1,4 +1,3 @@
-// frontend/src/features/PremiumPage/PremiumPage.jsx
 import React from 'react';
 import NewsFeed from '../NYInvestBorsa/NewsFeed.jsx';
 import YouTubeLinks from '../YoutubeLinks/YouTubeLinks.jsx';
@@ -6,17 +5,13 @@ import StockIndicators from '../NYInvestBorsa/StockIndicators.jsx';
 import './PremiumPage.css';
 
 const PremiumPage = () => {
-
-
   return (
     <div className="premium-page">
-      <div className="main-content">
-        <div className="youtube-links-container">
-          <YouTubeLinks />
-        </div>
-        <div className="stock-indicators-container">
-          <StockIndicators />
-        </div>
+      <div className="youtube-links-container">
+        <YouTubeLinks />
+      </div>
+      <div className="stock-indicators-container">
+        <StockIndicators />
       </div>
       <div className="news-feed-container">
         <NewsFeed />
--- frontend/src/features/YoutubeLinks/YouTubeLinks.jsx
+++ frontend/src/features/YoutubeLinks/YouTubeLinks.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import './YouTubeLinks.css';
 
 const YouTubeLinks = () => {
   const [videos, setVideos] = useState([]);
+  const videoListRef = useRef(null);
 
   useEffect(() => {
     const fetchVideos = async () => {
@@ -18,16 +19,26 @@ const YouTubeLinks = () => {
     fetchVideos();
   }, []);
 
+  const scroll = (direction) => {
+    const { current } = videoListRef;
+    if (current) {
+      const scrollAmount = current.offsetWidth / 4; // Adjust scroll amount based on the number of items visible
+      current.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="youtube-links">
       <h1>YouTube Videos</h1>
-      <div className="video-list">
+      {/* <div className="controls">
+        <button onClick={() => scroll(-1)}>&lt;</button>
+        <button onClick={() => scroll(1)}>&gt;</button>
+      </div> */}
+      <div className="video-list" ref={videoListRef}>
         {videos.map((video) => (
           <div key={video.id.videoId} className="video-item">
             <h3>{video.snippet.title}</h3>
             <iframe
-              width="100%"
-              height="400"
               src={`https://www.youtube.com/embed/${video.id.videoId}`}
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
